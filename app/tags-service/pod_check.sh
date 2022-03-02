269ec658ad33ed01b35f0ab67cb018ee5002ab53
Gustavo Soares <gustavo.nascimento-soares@imt-atlantique.net>
2022-03-02 15:26:30 +0100

    fix pod checks!!!

--- app/tags-service/pod_check.sh
+++ app/tags-service/pod_check.sh
@@ -5,7 +5,8 @@ while [ "$(kubectl get pods -l=app='tags-service' -o jsonpath='{.items[*].status
   if [[ $tries -gt $MAX_TRIES ]]; then
     kubectl describe pod tags
     exit 1
-  fi  sleep 5
+  fi
+  sleep 5
   echo "Waiting for tags pod to be ready."
   ((tries++))
 done
