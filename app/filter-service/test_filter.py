b70abd17ce1538923671d890aed23d2b3bc90aea
Gustavo Soares <gustavo.nascimento-soares@imt-atlantique.net>
2022-03-16 13:51:58 +0100

    agora vai mesmo, prometo

--- app/filter-service/test_filter.py
+++ app/filter-service/test_filter.py
@@ -6,7 +6,7 @@ from httpx import AsyncClient
 @pytest.mark.asyncio
 async def test_get_available_filters():
     async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post('/filters')
+        response = await ac.get('/filters')
         assert isinstance(response.json(), list)
         assert isinstance(response.json()[0], str)
         assert response.status_code == 200
