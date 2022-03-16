06d1110f75f7bd52eab1a4d775de266a0ca44f85
Gustavo Soares <gustavo.nascimento-soares@imt-atlantique.net>
2022-03-16 15:11:43 +0100

    converting image str to bytes

--- app/filter-service/filters.py
+++ app/filter-service/filters.py
@@ -4,15 +4,15 @@ from PIL.ImageFilter import BLUR, CONTOUR, SHARPEN
 
 
 def blur(bytes_: str) -> Image:
-    image = Image.open(BytesIO(bytes_))
+    image = Image.open(BytesIO(bytes_.encode('utf-8')))
     return image.filter(BLUR)
 
 
 def contour(bytes_: str) -> Image:
-    image = Image.open(BytesIO(bytes_))
+    image = Image.open(BytesIO(bytes_.encode('utf-8')))
     return image.filter(CONTOUR)
 
 
 def sharpen(bytes_: str) -> Image:
-    image = Image.open(BytesIO(bytes_))
+    image = Image.open(BytesIO(bytes_.encode('utf-8')))
     return image.filter(SHARPEN)
