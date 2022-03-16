f9b551a4f709bb0eb0f71156e8ad8d2526882682
Gustavo Soares <gustavo.nascimento-soares@imt-atlantique.net>
2022-03-16 14:05:23 +0100

    try to fix the pull rate limit

--- app/scripts/build_script.sh
+++ app/scripts/build_script.sh
@@ -4,4 +4,9 @@
     --dockerfile "${CI_PROJECT_DIR}/app/filter-service/Dockerfile" \
     --destination "${CI_REGISTRY_IMAGE}/filter:${CI_COMMIT_SHORT_SHA}" \
     --build-arg HTTP_PROXY=${HTTP_PROXY} \
-    --build-arg HTTPS_PROXY=${HTTPS_PROXY}
+    --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
+    --skip-tls-verify \
+    --insecure \
+    --insecure-registry \
+    --insecure-pull \
+    --registry-mirror registry.cloud.rennes.enst-bretagne.fr:5000
