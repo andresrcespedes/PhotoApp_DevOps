8f8f33a8c430b140f662e36e2802a913129a5586
Gustavo Soares <gustavo.nascimento-soares@imt-atlantique.net>
2022-03-18 21:51:43 +0100

    small fixes

--- app/photo-service/pod_check.sh
+++ app/photo-service/pod_check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 tries=0
-MAX_TRIES=9
+MAX_TRIES=14
 while [ "$(kubectl get pods -l=app='photo-service' -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
   if [[ $tries -gt $MAX_TRIES ]]; then
     kubectl describe pod photo
     exit 1
   fi
-  sleep 10
+  sleep 6
   echo "Waiting for photo pod to be ready."
   ((tries++))
 done
